b31ae85dd75223da7676041b9657aa038c803fbc
Fred Fettinger <fettinge@gmail.com>
2025-02-20 00:03:38 -0600

    home-assistant: add configuration configmap

---
+++ services/home-assistant/init.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+set -e
+
+# Copy the config from the configmap over the config in the persistent volume
+# This allows temporary edits for experimentation which are discarded when the pod restarts
+cp -r /config-templates /config
