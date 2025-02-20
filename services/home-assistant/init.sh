27f8972cf880d666a6717d5dd521f639e13cd706
Fred Fettinger <fettinge@gmail.com>
2025-02-20 01:08:13 -0600

    home-assistant: init: use cp -L to follow symbolic links

--- services/home-assistant/init.sh
+++ services/home-assistant/init.sh
@@ -3,4 +3,5 @@ set -e
 
 # Copy the config from the configmap over the config in the persistent volume
 # This allows temporary edits for experimentation which are discarded when the pod restarts
-mkdir -p /config && cp -r /config-templates/* /config
+# Busybox cp -R (recursive) preserves symlinks by default, use -L to disable
+mkdir -p /config && cp -RL /config-templates/* /config
