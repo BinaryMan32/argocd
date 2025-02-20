d37455a821d376514f6f30dedfc8b7475578b466
Fred Fettinger <fettinge@gmail.com>
2025-02-20 00:57:44 -0600

    Revert "use rsync to copy config files"
    rsync doesn't like the symbolic links used in k8s configmap mounts.
    
    This reverts commit 35dbe44ebb283050d094afcd0bc43e7cb9c6e7e7.

--- services/home-assistant/init.sh
+++ services/home-assistant/init.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 set -e
 
-apk add --no-cache rsync
-
 # Copy the config from the configmap over the config in the persistent volume
 # This allows temporary edits for experimentation which are discarded when the pod restarts
-rsync -rv /config-templates/ /config
+cp -r /config-templates /config
