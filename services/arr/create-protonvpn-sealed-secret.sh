4cc11b0dce400e57b144340bd28c06787aa93d8f
Fred Fettinger <fettinge@gmail.com>
2025-05-12 01:19:24 -0500

    arr: protonvpn: switch from creds to key

--- services/arr/create-protonvpn-sealed-secret.sh
+++ services/arr/create-protonvpn-sealed-secret.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/bash
 script_dir=$(dirname $(realpath $0))
-echo "Go to https://account.protonvpn.com/account-password#openvpn"
-read -e -p 'enter OpenVPN / IKEv2 username > ' protonvpn_username
-read -e -p 'enter OpenVPN / IKEv2 password > ' protonvpn_password
+echo "Go to https://account.proton.me/u/0/vpn/WireGuard"
+echo "1. Enter a name associated with this kubernetes cluster"
+echo "2. Change platform to 'GNU/Linux'"
+echo "3. Select VPN options NAT-PMP and VPN Accelerator"
+echo "4. Click 'Create' button"
+read -e -p 'enter PrivateKey > ' wireguard_private_key
 kubectl create secret generic \
     --dry-run=client \
     --namespace=arr \
     protonvpn-credentials \
-    --from-literal=username=$protonvpn_username \
-    --from-literal=password=$protonvpn_password \
+    --from-literal=wireguard_private_key=$wireguard_private_key \
     --output=yaml |
 kubeseal --format=yaml --sealed-secret-file=$script_dir/resources/sealed-secret-protonvpn-credentials.yaml
