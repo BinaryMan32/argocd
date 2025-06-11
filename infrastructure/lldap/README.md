9af8600672ca890d5ae5fc926ae14ac46ff67172
Fred Fettinger <fettinge@gmail.com>
2025-06-11 12:50:21 -0500

    add lldap

---
+++ infrastructure/lldap/README.md
@@ -0,0 +1,9 @@
+# LLDAP
+
+[LLDAP](https://github.com/lldap/lldap) is a Light LDAP implementation for authentication.
+
+## References
+
+- [LLDAP on Kubernetes](https://github.com/Evantage-WS/lldap-kubernetes)
+- [Creating a Bootstrap Job](https://github.com/lldap/lldap/blob/main/example_configs/bootstrap/bootstrap.md#kubernetes-job)
+- blog deploying [LLDAP on Kubernetes](https://blog.stonegarden.dev/articles/2025/01/lldap/)
---
+++ infrastructure/lldap/create-sealed-secrets.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/bash
+script_dir=$(dirname $(realpath $0))
+
+# https://github.com/lldap/lldap/blob/main/generate_secrets.sh
+print_random () {
+  LC_ALL=C tr -dc 'A-Za-z0-9!#%&()*+,-./:;<=>?@[\]^_{|}~' </dev/urandom | head -c 32
+}
+
+kubectl create secret generic \
+    --dry-run=client \
+    --namespace=auth \
+    lldap-crypto \
+    --from-literal=key_seed="$(print_random)" \
+    --from-literal=jwt_secret="$(print_random)" \
+    --output=yaml |
+kubeseal --format=yaml --sealed-secret-file=$script_dir/resources/sealed-secret-lldap-crypto.yaml
+
+kubectl create secret generic \
+    --dry-run=client \
+    --namespace=auth \
+    lldap-admin \
+    --from-literal=user_dn=admin \
+    --from-literal=user_pass="$(print_random)" \
+    --output=yaml |
+kubeseal --format=yaml --sealed-secret-file=$script_dir/resources/sealed-secret-lldap-admin.yaml
