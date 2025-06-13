2bc37f8295ee3d2d0b12441488fc76e864b2e53c
Fred Fettinger <fettinge@gmail.com>
2025-06-13 16:04:06 -0500

    authelia: generate random secrets only once

---
+++ infrastructure/authelia/create-sealed-secret-authelia.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+script_dir=$(dirname $(realpath $0))
+
+# https://github.com/lldap/lldap/blob/main/generate_secrets.sh
+print_random () {
+  LC_ALL=C tr -dc 'A-Za-z0-9' </dev/urandom | head -c 128
+}
+
+kubectl create secret generic \
+    --dry-run=client \
+    --namespace=auth \
+    authelia \
+    --from-literal=identity_validation.reset_password.jwt.hmac.key="$(print_random)" \
+    --from-literal=session.encryption.key="$(print_random)" \
+    --from-literal=storage.encryption.key="$(print_random)" \
+    --output=yaml |
+kubeseal --format=yaml --sealed-secret-file=$script_dir/templates/sealed-secret-authelia.yaml
