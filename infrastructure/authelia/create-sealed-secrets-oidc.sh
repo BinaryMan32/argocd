da1f3561e41c6634a676e97ad4015284a864f87c
Fred Fettinger <fettinge@gmail.com>
2025-06-18 20:44:57 -0500

    headlamp: remove HEADLAMP_CONFIG prefix from oidc secret keys

--- infrastructure/authelia/create-sealed-secrets-oidc.sh
+++ infrastructure/authelia/create-sealed-secrets-oidc.sh
@@ -2,5 +2,5 @@
 script_dir=$(dirname $(realpath $0))
 
 $script_dir/create-sealed-secret-oidc-helper.sh headlamp headlamp \
-  HEADLAMP_CONFIG_OIDC_CLIENT_ID HEADLAMP_CONFIG_OIDC_CLIENT_SECRET \
+  OIDC_CLIENT_ID OIDC_CLIENT_SECRET \
   ../headlamp/templates
