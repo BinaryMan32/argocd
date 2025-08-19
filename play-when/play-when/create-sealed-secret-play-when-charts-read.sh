ec07c26fffbc03c597267813780b31f5af787f9a
Fred Fettinger <fettinge@gmail.com>
2025-08-19 16:35:42 -0500

    add play-when-prod

--- play-when/play-when/create-sealed-secret-play-when-charts-read.sh
+++ play-when/play-when/create-sealed-secret-play-when-charts-read.sh
@@ -3,15 +3,21 @@ script_dir=$(dirname $(realpath $0))
 
 read -e -p 'enter Deploy token > ' deploy_token
 
-kubectl create secret generic \
-    --dry-run=client \
-    --namespace=argocd \
-    play-when-charts-read \
-    --from-literal=type=helm \
-    --from-literal=url=https://gitlab.com/api/v4/projects/46393971/packages/helm/stable \
-    --from-literal=project=play-when-stage \
-    --from-literal=username=argocd-read-helm \
-    --from-literal=password=$deploy_token \
-    --output=yaml |
-yq '.metadata.labels."argocd.argoproj.io/secret-type" = "repo-creds"' --yaml-output |
-kubeseal --format=yaml --sealed-secret-file=$script_dir/templates/sealed-secret-play-when-charts-read.yaml
+create_sealed_secret_helm() {
+    environment="$1"
+    kubectl create secret generic \
+        --dry-run=client \
+        --namespace=argocd \
+        $environment-charts-read \
+        --from-literal=type=helm \
+        --from-literal=url=https://gitlab.com/api/v4/projects/46393971/packages/helm/stable \
+        --from-literal=project=$environment \
+        --from-literal=username=argocd-read-helm \
+        --from-literal=password=$deploy_token \
+        --output=yaml |
+    yq '.metadata.labels."argocd.argoproj.io/secret-type" = "repo-creds"' --yaml-output |
+    kubeseal --format=yaml --sealed-secret-file=$script_dir/templates/$environment-sealed-secret-charts-read.yaml
+}
+
+create_sealed_secret_helm play-when-stage
+create_sealed_secret_helm play-when-prod
