9c90f8f9de1526bba2b9bf46e229238b06b8c786
Fred Fettinger <fettinge@gmail.com>
2026-02-17 17:17:17 -0600

    add seaweed-s3-config SealedSecret

---
+++ infrastructure/seaweed/create-sealed-secret-seaweed-s3-config.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/bash
+script_dir=$(dirname $(realpath $0))
+
+# https://github.com/lldap/lldap/blob/main/generate_secrets.sh
+print_random () {
+  LC_ALL=C tr -dc 'A-Za-z0-9' </dev/urandom | head -c $1
+}
+
+function generate_config() {
+  cat <<JSON
+{
+  "identities": [
+    {
+      "name": "Admin",
+      "actions": [
+        "Admin"
+      ],
+      "credentials": [
+        {
+          "accessKey": "$(print_random 32)",
+          "secretKey": "$(print_random 64)"
+        }
+      ]
+    },
+    {
+      "name": "ReadOnly",
+      "actions": [
+        "Read",
+        "List"
+      ],
+      "credentials": [
+        {
+          "accessKey": "$(print_random 32)",
+          "secretKey": "$(print_random 64)"
+        }
+      ]
+    }
+  ]
+}
+JSON
+}
+
+echo "$(generate_config)"
+
+kubectl create secret generic \
+    --dry-run=client \
+    --namespace=seaweed \
+    seaweed-s3-config \
+    --from-literal=s3-config.json="${s3_config_json}" \
+    --output=yaml |
+kubeseal --format=yaml --sealed-secret-file=$script_dir/resources/sealed-secret-seaweed-s3-config.yaml
