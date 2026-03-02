1bfa0d6988b4c7ed6c692c29293fbd373c7198e2
Fred Fettinger <fettinge@gmail.com>
2026-03-02 17:29:32 -0600

    argocd: add support for gitlab ingress

---
+++ infrastructure/argocd/create-webhook-sealed-secrets.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/bash
+script_dir=$(dirname $(realpath $0))
+
+# https://github.com/lldap/lldap/blob/main/generate_secrets.sh
+print_random () {
+  LC_ALL=C tr -dc 'A-Za-z0-9' </dev/urandom | head -c 64
+}
+
+create_webhook_sealed_secret() {
+    name="$1"
+    key="$2"
+    kubectl create secret generic \
+        --dry-run=client \
+        --namespace=argocd \
+        $name \
+        --from-literal=$key="$(print_random)" \
+        --output=yaml |
+    yq '.metadata.labels."app.kubernetes.io/part-of" = "argocd"' --yaml-output |
+    kubeseal --format=yaml --sealed-secret-file=$script_dir/$name-secret.yaml
+}
+
+create_webhook_sealed_secret webhook-gitlab secret
