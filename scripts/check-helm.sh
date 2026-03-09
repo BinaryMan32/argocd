4d2a745511b53650d4bb6bfef4fe466907163a30
Fred Fettinger <fettinge@gmail.com>
2026-03-09 15:30:50 -0500

    pass repository config to dependency build

--- scripts/check-helm.sh
+++ scripts/check-helm.sh
@@ -1,10 +1,11 @@
 #!/bin/bash -e
+REPOSITORY_CONFIG=${PWD}/helm-dependencies.yaml
 for chart in $(find . -name Chart.yaml); do
     echo "${chart}"
     chart_dir="$(dirname $chart)"
     if [ ! -d "${chart_dir}/charts" ] || [ ! -f "${chart_dir}/Chart.lock" ]; then
         echo "building helm dependencies"
-        helm dependency build "${chart_dir}"
+        helm dependency build "${chart_dir}" --repository-config "${REPOSITORY_CONFIG}"
     fi
-    helm template "$(basename "${chart_dir}")" "${chart_dir}" --values "${chart_dir}/values.yaml" --repository-config helm-dependencies.yaml --api-versions monitoring.coreos.com/v1 > /dev/null
+    helm template "$(basename "${chart_dir}")" "${chart_dir}" --values "${chart_dir}/values.yaml" --api-versions monitoring.coreos.com/v1 > /dev/null
 done
